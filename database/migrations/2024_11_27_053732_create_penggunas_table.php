e6e3e0968b5a588dfc87db3610acdfe573fa0065
Janaloka0 <marwansuhendra200@gmail.com>
2024-11-27 19:07:39 +0000

    memperbarui migration

--- database/migrations/2024_11_27_053732_create_penggunas_table.php
+++ database/migrations/2024_11_27_053732_create_penggunas_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('email');
             $table->string('password');
-            $table->string('role');
+            $table->enum('role',['admin','perugas kebun','manajer']);
             $table->timestamps();
         });
     }
