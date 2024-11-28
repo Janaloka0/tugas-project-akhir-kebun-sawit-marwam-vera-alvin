cf33ab556b8c1a62c318ac7341d5ded247b10ed6
Janaloka0 <marwansuhendra200@gmail.com>
2024-11-28 02:12:18 +0000

    menemabah seeder dan perbaikan migration

--- database/migrations/2024_11_27_053732_create_penggunas_table.php
+++ database/migrations/2024_11_27_053732_create_penggunas_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->enum('role',['admin','perugas kebun','manajer'])->nullable();
+            $table->enum('role',['admin','petugas kebun','manajer'])->nullable();
             $table->timestamps();
         });
     }
