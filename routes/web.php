8b4f87adf119deeb8c194e2a740ee582c0b7aaa7
Janaloka0 <marwansuhendra200@gmail.com>
2024-12-11 09:14:48 +0000

    membuat tampilan view

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ use App\Http\Controllers\PetugasController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\KebunController;
 
+
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:405770572.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3079282348.
+Route::get('/',function(){
+    return view('layout');
+});
 Route::post('/register', [PenggunaController::class, 'store']);
 // Route::post('/pengguna', 'PenggunaController@store');
 // Route::get('/pengguna', 'PenggunaController@index');
